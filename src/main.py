bd74d1096b90e3f655f7385a677e31a3fa3af208
somnoynadno <somnoynadno@yandex.ru>
2019-05-29 01:51:52 +0700

    Echo server 1.1

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
+import json
 import logging
 import random
+import pickle
 import pygame
 import socket
 import sys
+
 
 from pygame.locals import *
 
@@ -12,7 +15,7 @@ WINDOW_HEIGHT = 800
 
 # colors
 TEXT_COLOR = (255, 0, 0)  # red
-BACKGROUND_COLOR = (50, 100, 120)  # TODO: картинку на бэкграунд
+BACKGROUND_COLOR = (50, 100, 120)
 
 FPS = 40
 
@@ -24,9 +27,12 @@ BADDIE_MAX_SPEED = 8
 ADD_NEW_BADDIE_RATE = 6
 PLAYER_MOVE_RATE = 12
 
+sock = socket.socket()
 
 def terminate():
     pygame.quit()
+    sock.send("Connection closed".encode())
+    sock.close()
     sys.exit()
 
 
@@ -67,13 +73,161 @@ def open_server_protocol(host, port):
 
     # add handler to logger object
     logger.addHandler(fh)
-    sock = socket.socket()
-    sock.connect((host, port))
-    sock.send('Hello, world!'.encode())
 
-    data = sock.recv(1024)
-    print(data.decode())
-    sock.close()
+    sock.connect((host, port))
+
+    #
+    # test part goes here
+    #
+    top_score = 0
+    while True:
+        # set up the start of the game
+        baddies = []
+        score = 0
+        player_rect.topleft = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50)
+        move_left = move_right = move_up = move_down = False
+        reverse_cheat = slow_cheat = False
+        baddie_add_counter = 0
+        pygame.mixer.music.play(-1, 0.0)
+
+        while True:  # the game loop runs while the game part is playing
+            score += 1  # increase score
+
+            events = []
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    events.append((event.type, None))
+                    terminate()
+
+                if event.type == KEYDOWN:
+                    events.append((event.type, event.key))
+                    if event.key == ord('z'):
+                        reverse_cheat = True
+                    if event.key == ord('x'):
+                        slow_cheat = True
+                    if event.key == K_LEFT or event.key == ord('a'):
+                        move_right = False
+                        move_left = True
+                    if event.key == K_RIGHT or event.key == ord('d'):
+                        move_left = False
+                        move_right = True
+                    if event.key == K_UP or event.key == ord('w'):
+                        move_down = False
+                        move_up = True
+                    if event.key == K_DOWN or event.key == ord('s'):
+                        move_up = False
+                        move_down = True
+
+                if event.type == KEYUP:
+                    events.append((event.type, event.key))
+                    if event.key == ord('z'):
+                        reverse_cheat = False
+                        score = 0
+                    if event.key == ord('x'):
+                        slow_cheat = False
+                        score = 0
+                    if event.key == K_ESCAPE:
+                        terminate()
+
+                    if event.key == K_LEFT or event.key == ord('a'):
+                        move_left = False
+                    if event.key == K_RIGHT or event.key == ord('d'):
+                        move_right = False
+                    if event.key == K_UP or event.key == ord('w'):
+                        move_up = False
+                    if event.key == K_DOWN or event.key == ord('s'):
+                        move_down = False
+
+            json_string = json.dumps(events)
+
+            sock.sendall(json_string.encode())
+
+            data = sock.recv(1024)
+            print(data.decode())
+
+            # Add new baddies at the top of the screen, if needed.
+            if not reverse_cheat and not slow_cheat:
+                baddie_add_counter += 1
+            if baddie_add_counter == ADD_NEW_BADDIE_RATE:
+                baddie_add_counter = 0
+                baddie_size = random.randint(BADDIE_MIN_SIZE, BADDIE_MAX_SIZE)
+                new_baddie = {
+                    'rect': pygame.Rect(random.randint(0, WINDOW_WIDTH - baddie_size), 0 - baddie_size, baddie_size,
+                                        baddie_size),
+                    'speed': random.randint(BADDIE_MIN_SPEED, BADDIE_MAX_SPEED),
+                    'surface': pygame.transform.scale(enemy_image, (baddie_size, baddie_size)),
+                }
+
+                baddies.append(new_baddie)
+
+            # Move the player around.
+            if move_left and player_rect.left > 0:
+                player_rect.move_ip(-1 * PLAYER_MOVE_RATE, 0)
+            if move_right and player_rect.right < WINDOW_WIDTH:
+                player_rect.move_ip(PLAYER_MOVE_RATE, 0)
+            if move_up and player_rect.top > 0:
+                player_rect.move_ip(0, -1 * PLAYER_MOVE_RATE)
+            if move_down and player_rect.bottom < WINDOW_HEIGHT:
+                player_rect.move_ip(0, PLAYER_MOVE_RATE)
+
+            # Move the mouse cursor to match the player.
+            pygame.mouse.set_pos(player_rect.centerx, player_rect.centery)
+
+            # Move the baddies down.
+            for b in baddies:
+                if not reverse_cheat and not slow_cheat:
+                    b['rect'].move_ip(0, b['speed'])
+                elif reverse_cheat:
+                    b['rect'].move_ip(0, -5)
+                elif slow_cheat:
+                    b['rect'].move_ip(0, 1)
+
+            # Delete baddies that have fallen past the bottom.
+            for b in baddies[:]:
+                if b['rect'].top > WINDOW_HEIGHT:
+                    baddies.remove(b)
+
+            # Draw background
+            background_image = pygame.image.load("../drawable/backgrounds/background1.jpg")
+            background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
+
+            window_surface.blit(background_image, [0, 0])
+
+            # Draw the score and top score.
+            draw_text('Score: %s' % (score), font, window_surface, 10, 0)
+            draw_text('Top Score: %s' % (top_score), font, window_surface, 10, 40)
+
+            # Draw the player's rectangle
+            window_surface.blit(player_image, player_rect)
+
+            # Draw each baddie
+            for b in baddies:
+                window_surface.blit(b['surface'], b['rect'])
+
+            pygame.image.save(window_surface, "temp.jpg")
+            pygame.display.update()
+
+            # Check if any of the baddies have hit the player.
+            if player_has_hit_baddie(player_rect, baddies):
+                if score > top_score:
+                    top_score = score  # set new top score
+                break
+
+            main_clock.tick(40)
+
+        # Stop the game and show the "Game Over" screen.
+        pygame.mixer.music.stop()
+        game_over_sound.play()
+
+        draw_text('GAME OVER', font, window_surface, (WINDOW_WIDTH / 3), (WINDOW_HEIGHT / 3))
+        draw_text('Press a key to play again.', font, window_surface, (WINDOW_WIDTH / 3) - 80, (WINDOW_HEIGHT / 3) + 50)
+        pygame.display.update()
+        wait_for_player_to_press_key()
+
+        game_over_sound.stop()
+    #
+    # end of test part
+    #
 
 
 def game_loop():
@@ -178,17 +332,15 @@ def game_loop():
                 if b['rect'].top > WINDOW_HEIGHT:
                     baddies.remove(b)
 
-            # Draw the game world on the window.
-            window_surface.fill(BACKGROUND_COLOR)
-
-            # Draw the score and top score.
-            draw_text('Score: %s' % (score), font, window_surface, 10, 0)
-            draw_text('Top Score: %s' % (top_score), font, window_surface, 10, 40)
-
+            # Draw background
             background_image = pygame.image.load("../drawable/backgrounds/background1.jpg")
             background_image = pygame.transform.scale(background_image, (WINDOW_WIDTH, WINDOW_HEIGHT))
 
             window_surface.blit(background_image, [0, 0])
+
+            # Draw the score and top score.
+            draw_text('Score: %s' % (score), font, window_surface, 10, 0)
+            draw_text('Top Score: %s' % (top_score), font, window_surface, 10, 40)
 
             # Draw the player's rectangle
             window_surface.blit(player_image, player_rect)
@@ -226,6 +378,7 @@ if __name__ == "__main__":
     # set up pygame, the window, and the mouse cursor
     pygame.init()
     main_clock = pygame.time.Clock()
+    # window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.FULLSCREEN)
     window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
     background_image = pygame.image.load("../drawable/backgrounds/main_menu.jpg")
@@ -258,7 +411,7 @@ if __name__ == "__main__":
 
     # TODO: здесь отрисовать меню
 
-    if True:
+    if False:
         wait_for_player_to_press_key()
         game_loop()
     else:
--- src/server.py
+++ src/server.py
@@ -1,13 +1,21 @@
-"""
-Our future server
-"""
-
+import json
 import logging
+import pygame
 import socket
-import time
+import sys
+
+from pygame.locals import *
 
 HOST = '0.0.0.0'
 PORT = 9027
+sock = socket.socket()
+
+
+def terminate():
+    pygame.quit()
+    sock.close()
+    sys.exit()
+
 
 logger = logging.getLogger("Fucking Server")
 logger.setLevel(logging.INFO)
@@ -21,28 +29,77 @@ fh.setFormatter(formatter)
 # add handler to logger object
 logger.addHandler(fh)
 
-sock = socket.socket()
 
 sock.bind((HOST, PORT))
-sock.listen(2)
+sock.listen(100)
 
 print("Hello from server")
 logger.info('Start serving on %s' % (PORT))
 
+conn1, address1 = sock.accept()
+logger.info('Connection first from %s on %s' % (address1[0], address1[1]))
+
+# conn2, address2 = sock.accept()
+# logger.info('Connection second from %s on %s' % (address2[0], address2[1]))
+
 while True:
-    conn, address = sock.accept()
-    data = conn.recv(1024)
+    data = conn1.recv(1024).decode()
 
-    logger.info('Connection from %s on %s' % (address[0], address[1]))
+    if data:
+        if data == "Connection closed":
+            print(data)
+            break
 
-    print('Connection:', address)
-    print('------------------------------')
-    print("Request Data")
-    print('------------------------------')
-    print(data.decode())
+        print('Connection:', address1)
+        print('------------------------------')
+        print("Request Data")
+        print('------------------------------')
+        print(data)
 
-    conn.send(data)
-    conn.close()
+        data = json.loads(data)
 
-    # do not load the CPU
-    time.sleep(0.1)
+        for event_type, event_key in data:
+            if event_type == QUIT:
+                terminate()
+
+            if event_type == KEYDOWN:
+                if event_key == ord('z'):
+                    reverse_cheat = True
+                if event_key == ord('x'):
+                    slow_cheat = True
+                if event_key == K_LEFT or event_key == ord('a'):
+                    move_right = False
+                    move_left = True
+                if event_key == K_RIGHT or event_key == ord('d'):
+                    move_left = False
+                    move_right = True
+                if event_key == K_UP or event_key == ord('w'):
+                    move_down = False
+                    move_up = True
+                if event_key == K_DOWN or event_key == ord('s'):
+                    move_up = False
+                    move_down = True
+
+            if event_type == KEYUP:
+                if event_key == ord('z'):
+                    reverse_cheat = False
+                    score = 0
+                if event_key == ord('x'):
+                    slow_cheat = False
+                    score = 0
+                if event_key == K_ESCAPE:
+                    terminate()
+
+                if event_key == K_LEFT or event_key == ord('a'):
+                    move_left = False
+                if event_key == K_RIGHT or event_key == ord('d'):
+                    move_right = False
+                if event_key == K_UP or event_key == ord('w'):
+                    move_up = False
+                if event_key == K_DOWN or event_key == ord('s'):
+                    move_down = False
+
+        data = json.dumps(data)
+        conn1.send(data.encode())
+
+logger.info('Connection from %s on %s closed' % (address1[0], address1[1]))
