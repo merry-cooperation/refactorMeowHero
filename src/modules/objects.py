21c44dba9c8611005ab706eb1d0409434b7a361d
heabyfik <rafikova0rr@gmail.com>
2019-06-21 12:24:58 +0700

    6 lvl-fix images

--- src/modules/objects.py
+++ src/modules/objects.py
@@ -503,7 +503,7 @@ class Teacher(Boss):
     def __init__(self, name, level):
         super().__init__(name, level)
 
-        self.w = int(WINDOW_WIDTH / 10)
+        self.w = int(WINDOW_WIDTH / 14)
         self.h = int(WINDOW_HEIGHT / 8)
 
         try:
