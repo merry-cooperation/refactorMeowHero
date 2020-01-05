1e9b0a1b87fa4b898b93b6d2c1839d0433ef82cc
Oleg Abramov <abramov.o.v@gmail.com>
2020-01-05 16:36:49 +0700

    added tests and test summary for #27

--- src/tests/README.md
+++ src/tests/README.md
@@ -7,9 +7,11 @@ conditional complexity|[#7][i7]| - | - | Поскольку код с недос
 data clump|[#18][i18]|[link](test_interface.py#L7-L41)|8|Использованы mock'и
 lazy class|[#28][i28]| - | - | Использование ленивого класса находится внутри огромного бесконечного цикла, поэтому написать юнит тесты не получается
 shotgun surgery|[#1][i1]| - | - | В силу того, что в недостатке речь о глобальных константах, написать юнит тест не получится
+duplicate code|[#27][i27]|[link](test_objects.py#L7-L79)|12|Использованы mock'и
 
 [i1]: https://github.com/merry-cooperation/refactorMeowHero/issues/1
 [i7]: https://github.com/merry-cooperation/refactorMeowHero/issues/7
 [i17]: https://github.com/merry-cooperation/refactorMeowHero/issues/17
 [i18]: https://github.com/merry-cooperation/refactorMeowHero/issues/18
+[i27]: https://github.com/merry-cooperation/refactorMeowHero/issues/27
 [i28]: https://github.com/merry-cooperation/refactorMeowHero/issues/28
---
+++ src/tests/test_objects.py
@@ -0,0 +1,87 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import patch, Mock
+from modules.objects import MeowHero, Health, Bullet
+
+
+@patch('modules.objects.pygame.transform.scale', autospec=True)
+@patch('modules.objects.pygame.image.load', autospec=True)
+class TestMeowHero(TestCase):
+
+    def test_init_calls_pygame_image_load_2times(self, mock_image_load, mock_transform_scale):
+        meow_hero = MeowHero(1)
+        self.assertEqual(mock_image_load.call_count, 2)
+
+    def test_init_calls_pygame_transform_scale_2times(self, mock_image_load, mock_transform_scale):
+        meow_hero = MeowHero(1)
+        self.assertEqual(mock_transform_scale.call_count, 2)
+
+    def test_init_calls_get_rect_2times(self, mock_image_load, mock_transform_scale):
+        mock_get_rect = Mock()
+        mock_transform_scale.return_value.get_rect = mock_get_rect
+
+        meow_hero = MeowHero(1)
+        self.assertEqual(mock_get_rect.call_count, 2)
+
+
+@patch('modules.objects.pygame.transform.scale', autospec=True)
+@patch('modules.objects.pygame.image.load', autospec=True)
+class TestHealth(TestCase):
+
+    def test_init_calls_pygame_image_load_once(self, mock_image_load, mock_transform_scale):
+        health = Health(1, 10, 10)
+        mock_image_load.assert_called_once()
+
+    def test_init_calls_pygame_transform_scale_once(self, mock_image_load, mock_transform_scale):
+        health = Health(1, 10, 10)
+        mock_transform_scale.assert_called_once()
+
+    def test_init_calls_get_rect_once(self, mock_image_load, mock_transform_scale):
+        mock_get_rect = Mock()
+        mock_transform_scale.return_value.get_rect = mock_get_rect
+
+        health = Health(1, 10, 10)
+        mock_get_rect.assert_called_once()
+
+
+@patch('modules.objects.pygame.transform.scale', autospec=True)
+@patch('modules.objects.pygame.image.load', autospec=True)
+class TestBullet(TestCase):
+
+    def test_init_simple_calls_pygame_image_load_once(self, mock_image_load, mock_transform_scale):
+        bullet = Bullet(1, "Simple")
+        mock_image_load.assert_called_once()
+
+    def test_init_simple_calls_pygame_transform_scale_once(self, mock_image_load, mock_transform_scale):
+        bullet = Bullet(1, "Simple")
+        mock_transform_scale.assert_called_once()
+
+    def test_init_simple_calls_get_rect_once(self, mock_image_load, mock_transform_scale):
+        mock_get_rect = Mock()
+        mock_transform_scale.return_value.get_rect = mock_get_rect
+
+        bullet = Bullet(1, "Simple")
+        mock_get_rect.assert_called_once()
+
+    def test_init_multiplayer_calls_pygame_image_load_once(self, mock_image_load, mock_transform_scale):
+        bullet = Bullet(1, "Multiplayer")
+        mock_image_load.assert_called_once()
+
+    def test_init_multiplayer_calls_pygame_transform_scale_once(self, mock_image_load, mock_transform_scale):
+        bullet = Bullet(1, "Multiplayer")
+        mock_transform_scale.assert_called_once()
+
+    def test_init_multiplayer_calls_get_rect_once(self, mock_image_load, mock_transform_scale):
+        mock_get_rect = Mock()
+        mock_transform_scale.return_value.get_rect = mock_get_rect
+
+        bullet = Bullet(1, "Multiplayer")
+        mock_get_rect.assert_called_once()
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
