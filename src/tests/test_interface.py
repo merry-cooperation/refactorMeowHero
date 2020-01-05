6253619cb14cb770a4130839f54f0f4796459cf3
Oleg Abramov <abramov.o.v@gmail.com>
2020-01-05 16:41:28 +0700

    refactored test code for #18

--- src/tests/test_interface.py
+++ src/tests/test_interface.py
@@ -4,41 +4,44 @@ from unittest.mock import patch
 from modules.interface import Button
 
 
+@patch('modules.interface.pygame.transform.scale')
+@patch('modules.interface.pygame.image.load')
+@patch('modules.interface.pygame.font.SysFont')
 class TestButton(TestCase):
-    @patch('modules.interface.pygame.transform.scale')
-    @patch('modules.interface.pygame.image.load')
-    @patch('modules.interface.pygame.font.SysFont')
-    def setUp(self, mock_sys_font, mock_image_load, mock_transform_scale):
-        self.button = Button(1, 2, 10, 20)
-        self.mock_sys_font = mock_sys_font
-        self.mock_image_load = mock_image_load
-        self.mock_transform_scale = mock_transform_scale
 
-    def test_init_calls_pygame_font_SysFont_once(self):
-        self.mock_sys_font.assert_called_once()
+    def test_init_calls_pygame_font_SysFont_once(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        mock_sys_font.assert_called_once()
 
-    def test_init_calls_pygame_image_load_3times(self):
-        self.assertEqual(self.mock_image_load.call_count, 3)
+    def test_init_calls_pygame_image_load_3times(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        self.assertEqual(mock_image_load.call_count, 3)
 
-    def test_init_calls_pygame_transform_scale_3times(self):
-        self.assertEqual(self.mock_transform_scale.call_count, 3)
+    def test_init_calls_pygame_transform_scale_3times(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        self.assertEqual(mock_transform_scale.call_count, 3)
 
-    def test_init_sets_x(self):
-        self.assertEqual(self.button.x, 1)
+    def test_init_sets_x(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        self.assertEqual(button.x, 1)
 
-    def test_init_sets_y(self):
-        self.assertEqual(self.button.y, 2)
+    def test_init_sets_y(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        self.assertEqual(button.y, 2)
 
-    def test_init_sets_w(self):
-        self.assertEqual(self.button.w, 10)
+    def test_init_sets_w(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        self.assertEqual(button.w, 10)
 
-    def test_init_sets_h(self):
-        self.assertEqual(self.button.h, 20)
+    def test_init_sets_h(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        self.assertEqual(button.h, 20)
 
-    def test_init_passing_wh_to_pygame_transform_scale(self):
-        for call_args in self.mock_transform_scale.call_args_list:
+    def test_init_passing_wh_to_pygame_transform_scale(self, mock_sys_font, mock_image_load, mock_transform_scale):
+        button = Button(1, 2, 10, 20)
+        for call_args in mock_transform_scale.call_args_list:
             with self.subTest(call_args=call_args):
-                self.assertEqual(call_args[0][1], (self.button.w, self.button.h))
+                self.assertEqual(call_args[0][1], (button.w, button.h))
 
 
 def main():
